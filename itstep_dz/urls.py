dbcafc79a0cd303ced763bee834c6a5727f39b05
SlavaMokrynskyi <chempionslava@gmail.com>
2025-12-20 10:02:44 +0200

    add home page

--- itstep_dz/urls.py
+++ itstep_dz/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
-    path('',include('products.urls')),
-    path('admin-panel/',include('admin_panel.urls')),
 ]
