a482465a4816724c130a73c540a0faf95b408618
SlavaMokrynskyi <chempionslava@gmail.com>
2026-01-24 12:15:07 +0200

    filter by category

--- home/views.py
+++ home/views.py
@@ -7,17 +7,21 @@ def index(request, filter_by_favorites=False):
     products = Product.objects.all()
 
     filter_text = request.GET.get("filter_search", "")
-
+    selected_category = request.GET.get('category', 'all')
+    
     if filter_by_favorites:
         fav_ids = get_favorite_products(request)
         products = products.filter(id__in=fav_ids)
 
     if filter_text:
-            products = products.filter(name__icontains=filter_text)
-            
+        products = products.filter(name__icontains=filter_text)
+
+    if selected_category != 'all' and selected_category:
+        products = products.filter(category=selected_category)
+
     return render(request, 'home/index.html', {
         'products': products,
-        "filter_by_favorites": filter_by_favorites,
-        "fav_count": get_count_of_favorite_products(request),
-        "fav_ids": get_favorite_products(request),
+        'filter_by_favorites': filter_by_favorites,
+        'selected_category': selected_category,  # Додано цей рядок
+        'fav_ids': get_favorite_products(request),
     })
