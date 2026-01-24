ecd389b275a4b67a1cef281b1b4ed69333064a4a
SlavaMokrynskyi <chempionslava@gmail.com>
2026-01-24 11:04:58 +0200

    search fix

--- home/views.py
+++ home/views.py
@@ -6,12 +6,15 @@ from favorites.favorites import get_favorite_products, get_count_of_favorite_pro
 def index(request, filter_by_favorites=False):
     products = Product.objects.all()
 
-    filter_text = request
+    filter_text = request.GET.get("filter_search", "")
 
     if filter_by_favorites:
         fav_ids = get_favorite_products(request)
         products = products.filter(id__in=fav_ids)
 
+    if filter_text:
+            products = products.filter(name__icontains=filter_text)
+            
     return render(request, 'home/index.html', {
         'products': products,
         "filter_by_favorites": filter_by_favorites,
